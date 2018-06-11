b329f54918d06a03d1fd90ec5c7d7faa7a140a97
Radek Ostrowski <radek@toptal.com>
2018-06-11 15:15:06 +1000

    added test for receiveApproval

--- test/DreamCoinTest.js
+++ test/DreamCoinTest.js
@@ -1,10 +1,16 @@
+var utils = require('web3-utils');
+
 const MusicCoin = artifacts.require('./MusicCoin.sol');
+const MusicSeconds = artifacts.require('./MusicSeconds.sol');
 
 let owner;
 let feeAccount;
+let hacker;
 
 let coin;
-let rate = 100;
+let second;
+let rate = 1;
+let framesLimit = 100;
 
 let oneHundredTokens = web3.toWei(100, "ether");
 let fiftyTokens = web3.toWei(50, "ether");
@@ -16,15 +22,36 @@ contract('DreamCoinTest', (accounts) => {
     beforeEach(async () => {
         owner = accounts[0];
         feeAccount = accounts[1];
+        hacker = accounts[2];
 
         coin = await MusicCoin.new(feeAccount, rate);
+        second = await MusicSeconds.new("name", "symbol", framesLimit, coin.address);
     });
+
+    function expectRevert(e, msg) {
+        assert(e.message.search('revert') >= 0, msg);
+    }
 
     it("buy coins works correctly", async () => {
         await coin.buy({from: owner, value: oneEther});
 
         let balance = await coin.balanceOf(owner);
         assert(balance.toNumber() == web3.toWei(rate, "ether"));
+    });
+
+
+    it("cannot claim seconds by calling directly receiveApproval without allowance", async () => {
+        const trackId = 2;
+        assert(false == await second.exists.call(trackId), "token should not be owned by anyone");
+        
+        try {
+            await second.receiveApproval(hacker, oneHundredTokens, coin.address, utils.toHex(trackId), {from: hacker});
+            assert(false);
+        } catch (e) {
+            expectRevert(e, "no allowance provided");
+        }
+
+        assert(false == await second.exists.call(trackId), "token still should not be owned by anyone");
     });
   
 })
